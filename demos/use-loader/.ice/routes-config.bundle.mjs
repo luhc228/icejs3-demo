c076f34e3e1e13e220e397eb7c0a4724775f9816
luhc228 <luhengchang228@126.com>
2023-02-06 23:54:40 +0800

    chore: remove tmp files

--- demos/use-loader/.ice/routes-config.bundle.mjs
+++
@@ -1,5 +0,0 @@
-// .ice/routes-config.ts
-var routes_config_default = {};
-export {
-  routes_config_default as default
-};
--- demos/use-loader/build/server/pages-BDW4OXVL.mjs
+++
@@ -1,2 +0,0 @@
-import { createRequire } from 'module';const require = createRequire(import.meta.url);
-//# sourceMappingURL=pages-BDW4OXVL.mjs.map
